a243c2a18cead4ac6c31e1ca2474726c9fb1d1c5
Room <room@Rooms-MacBook-Pro.local>
2019-11-29 19:12:31 +0700

    create buoi1

---
+++ buoi1.js
@@ -0,0 +1,9 @@
+hello
+//Quan li local
+//Tao thu muc .git: git init
+//Nhan command + shift + . để hiện thư mục ẩn
+//Nhan commang + shift + ~ để hiện thị cua so dong lenh
+//**Luu thay doi */ 
+//kiem tra trang thai: git status
+//
+
