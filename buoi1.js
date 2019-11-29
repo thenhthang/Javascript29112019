7d0af343a5df942c28aa1fb6ca3c4e814e6c163f
Room <room@Rooms-MacBook-Pro.local>
2019-11-29 19:13:46 +0700

    sua file buoi 1

--- buoi1.js
+++ buoi1.js
@@ -5,5 +5,6 @@ hello
 //Nhan commang + shift + ~ để hiện thị cua so dong lenh
 //**Luu thay doi */ 
 //kiem tra trang thai: git status
+//luu vao git tam: git add tenfile hoac git add . de luu tat ca
+//commit : git commit -m "noi dung ghi chu"
 //
-
