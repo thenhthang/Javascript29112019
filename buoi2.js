91f3c031ae09cd840ce53b01e20aa1aedae6aa6e
thenhthang <quocntrivn@ymail.com>
2019-12-02 18:54:27 +0700

    update lan 6

---
+++ buoi2.js
@@ -0,0 +1,18 @@
+//Javascript
+//1: Khai bao bien
+//Cai dat goi Tu dong chay: npm install nodemon -g
+//khoi dong: nodemon tenfile.js
+/*var teo = "Nguyen van teo"
+teo = 20
+let age = 30
+age = 1000
+console.log(teo)
+console.log(age)
+const number = 5*/
+//2. Kiem du lieu
+//nguyen thuy: primities
+// String Number Boolean null undefined symbol 
+var aa = null
+var bb
+console.log(aa)
+console.log(bb)
