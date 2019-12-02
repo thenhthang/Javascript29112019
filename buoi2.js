323cbc87cd317e95c13b2af9382b9808fa13577d
thenhthang <quocntrivn@ymail.com>
2019-12-02 19:17:29 +0700

    array

--- buoi2.js
+++ buoi2.js
@@ -10,9 +10,27 @@ console.log(teo)
 console.log(age)
 const number = 5*/
 //2. Kiem du lieu
+//Const: Khong thay doi duoc gia tri cua noi nhung thay doi duoc gia tri cua con no vi: trong mot arry
 //nguyen thuy: primities
 // String Number Boolean null undefined symbol 
-var aa = null
+/*var aa = null
 var bb
 console.log(aa)
-console.log(bb)
+console.log(bb)*/
+// 3: Object
+//Truong hop : Object
+//Nằm trong ngoac {}, nen su dung const
+// const teo = {
+//     name    : "Nguyen van toe",
+//     age :   15,
+//     full: {
+//         ga: "qua ga"
+//     }
+// }
+// console.log(teo)
+// 4: Array
+const arrNames = ["Teo","Ti","Tun"]
+console.log(arrNames)
+console.log(arrNames[1])
+arrNames[1] = "Titi"
+console.log(arrNames[1])
