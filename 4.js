f6e49dc63bfc0416186f95671302b470afb4fa12
thenhthang <quocntrivn@ymail.com>
2019-12-06 18:58:45 +0700

    Array method

--- 4.js
+++ 4.js
@@ -2,13 +2,28 @@
 //null la Object
 //function la function
 //undefined la undefined
-var number = 5, 
-name = "Tran van teo",
-arr = [1,2,3,4,5],
-specialundefined = undefined,
-specialNull = null
-console.log(typeof number,
-    typeof name,
-    typeof arr, 
-    typeof specialundefined,
-    typeof specialNull)
+// var number = 5, 
+// name = "Tran van teo",
+// arr = [1,2,3,4,5],
+// specialundefined = undefined,
+// specialNull = null
+// console.log(typeof number,
+//     typeof name,
+//     typeof arr, 
+//     typeof specialundefined,
+//     typeof specialNull)
+
+//** 13. Array method */
+const numbers = [1,2,3,4,5]
+//1. Them phan tu vao vi tri cuoi cung
+//numbers.push(6,7,8,"haha")
+//console.log(numbers)
+//2. Xoa phan tu vi tri cuoi cung
+// var value = numbers.pop()
+//console.log(value)
+//3. Them phan tu vi tri dau tien
+// numbers.unshift(-1,0)
+// console.log(numbers)
+//4. Xoa phan tu vi tri dau tien
+numbers.shift()
+console.log(numbers)
