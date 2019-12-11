2c208a8af92599e5aef492ff944a1bf3bc23e4ae
thenhthang <quocntrivn@ymail.com>
2019-12-11 18:08:44 +0700

    Built-in function

---
+++ 6.js
@@ -0,0 +1,14 @@
+//**Built-in function : function da duoc xay dung san, chi goi lai*/
+// Ham Random
+// 5 - 11
+const number = (Math.random() * 6) + 5
+console.log(number)
+// Ham Round: duoi 0.5 lam tron xuong, lon hon hoac bang 0.5 lam tron len
+var number1 = Math.round(number)
+console.log(number1)
+// Ham lam tron xuong
+console.log(Math.floor(number))
+//Ham lan tron len
+console.log(Math.ceil(number))
+//Ham can bac 2
+console.log(Math.sqrt(number))
