ae921548f8946739949daa582264d58ee46c86a4
Thênh Thang <quocntrivn@ymail.com>
2019-12-13 14:54:40 +0700

    Create save.js

---
+++ save.js
@@ -0,0 +1,12 @@
+const arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+Array.prototype.mymap = function (callBackFun) {
+  var output = []
+  for (let i = 0; i < this.length; i++) {
+    //output.push(callBackFun(this[i]))
+    output[i] = callBackFun(this[i])
+  }
+  return output
+}
+console.log(arrNumbers.mymap(function (value) {
+  return value * 2
+}))
